c3e4de4486458493175df17b8e1b980584832c0e
Shrijan Lakhey <lakheyshrijan@gmail.com>
2023-09-27 19:26:42 +0545

    installed simpleJWT package and made changes to the urlpatterns and settings as described in the documentation

--- backend-auth/user_api/views.py
+++ backend-auth/user_api/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 
-# Create your views here.
+# available routes for using this api
 @api_view(['GET'])
 def getRoutes(request):
     routes=[
